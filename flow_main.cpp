7920bc8806dacd8dd861f31890d1f3b99cde351f
Dylan Palmieri <dgpalmieri@gmail.com>
2020-12-12 20:31:39 -0900

    Add GFG test case

--- flow_main.cpp
+++ flow_main.cpp
@@ -5,9 +5,46 @@
 
 #include"flow.hpp"
 
+#include<iostream>
+using std::cout;
+using std::endl;
+
 int main() {
+
     Flow test;
     test.hello();
+
+    { // first test case with example from
+      // https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/
+
+    // The format for the graph is:
+    // index = node number
+    // pair.first = node the edge connectes to
+    // pair.second = edge weight
+    Flow gfg_test( {
+                 { { 1, 16 }, { 2, 13 } }, // So, we have an edge 0->1 with weight 16, etc.
+                 { { 2, 10 }, { 3, 12 } },
+                 { { 1,  4 }, { 4, 14 } },
+                 { { 2,  9 }, { 5, 10 } },
+                 { { 3,  7 }, { 5,  4 } },
+                 { }
+                 } );
+
+    gfg_test.calculate();
+    int tFlow = gfg_test.getMaxFlow();
+
+    cout << "First test case: graph from GeeksForGeeks example" << endl;
+    gfg_test.printGraph();
+    if ( tFlow != 23 ){
+        cout << "   TEST FAILED" << endl;
+        cout << "   Max Flow value calculated: " << tFlow << endl;
+        cout << "   Expected value: 23" << endl;
+
+    }
+    else
+        cout << "   Test Passed!" << endl;
+
+    } // end first test case
 
     return 0;
 }
