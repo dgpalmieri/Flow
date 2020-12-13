211f7a958dffd9aa41a2725a3210cf1fdb7359fd
Dylan Palmieri <dgpalmieri@gmail.com>
2020-12-12 20:34:17 -0900

    Add skeleton functions

--- flow.cpp
+++ flow.cpp
@@ -7,8 +7,33 @@
 #include"flow.hpp"
 
 #include<iostream>
+using std::cout;
+using std::endl;
+
+#include<utility>
+using std::pair;
+
+#include<string>
+using std::string;
 
 void Flow::hello() const {
     std::cout << "Hello, World!" << std::endl;
 }
 
+void Flow::printGraph() const {
+    for ( size_t i = 0; i < this->_graph.size(); ++i ){
+            cout << "vertex " << i << ": ";
+        for ( auto x : this->_graph[i] ){
+            cout << x.first << "(" << x.second << ") ";
+        }
+        cout << endl;
+    }
+}
+
+int Flow::getMaxFlow() { return this->_maxFlow; }
+
+bool bfs( string & path, int & weight );
+
+void Flow::calculate() {
+    this->_maxFlow = 0;
+}
--- flow.hpp
+++ flow.hpp
@@ -6,10 +6,32 @@
 #ifndef FLOW_HPP
 #define FLOW_HPP
 
+#include<vector>
+#include<utility>
+
+using Graph = std::vector < std::vector< std::pair< int, int > > >;
+
 class Flow {
+
     public:
 
+        Flow() {};
+
+        Flow( Graph graph ) : _graph(graph) { };
+
         void hello() const;
+
+        void printGraph() const;
+
+        void calculate();
+
+        int getMaxFlow();
+
+    private:
+
+        Graph _graph;
+
+        int _maxFlow;
 
 }; // End class Flow
 
