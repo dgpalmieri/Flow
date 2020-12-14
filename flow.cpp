44881328f3d82b983d41d26cec9130213717fbb4
Dylan Palmieri <dgpalmieri@gmail.com>
2020-12-13 15:03:40 -0900

    Implement bfs - untested

--- flow.cpp
+++ flow.cpp
@@ -12,9 +12,19 @@ using std::endl;
 
 #include<utility>
 using std::pair;
+using std::make_pair;
+
+#include<vector>
+using std::vector;
 
 #include<string>
 using std::string;
+
+#include<queue>
+using std::queue;
+
+#include<unordered_set>
+using std::unordered_set;
 
 void Flow::hello() const {
     std::cout << "Hello, World!" << std::endl;
@@ -34,8 +44,63 @@ int Flow::getMaxFlow() { return this->_maxFlow; }
 
 void Flow::setGraph( Graph & graph ) { this->_graph = graph; }
 
-bool bfs( string & path, int & weight );
+// bool bfs
+// Pre: all parameters are initialized
+// Post:
+//      path contains a list of edges of a path from source to sink
+//      weight contains the maximum possible flow flow through the path
+bool bfs( vector< pair< int, int > > & path, int & weight, const Graph & g){
+    int currentVertex = 0;
+    queue< int > nextVerticies;
+    unordered_set< int > discovered{ 0 };
+
+    while ( g[ currentVertex ].size() > 0 ) {
+
+        for ( auto p : g[currentVertex] )
+            if ( p.second > 0 &&
+                 discovered.find( p.first ) == discovered.end() ) {
+                nextVerticies.push( p.first );
+                discovered.insert( p.first );
+                weight += p.second;
+            }
+
+        int nextVertex = nextVerticies.front();
+        nextVerticies.pop();
+
+        path.push_back( { currentVertex, nextVertex } );
+
+        currentVertex = nextVertex;
+    }
+
+    auto end = path.size() - 1;
+    auto prev = end - 1;
+    while ( true ) {
+        if ( prev < 0 ) { break; }
+
+        if ( path[ prev ].second != path[ end ].first ) {
+            for ( auto p : g[ path[ prev ].first ] ) {
+                if ( p.first == path[ prev ].second ) {
+                    weight -= p.second;
+                    break;
+                }
+            }
+            path.erase( path.begin() + prev );
+        }
+
+        --end;
+        --prev;
+    }
+
+    return true;
+}
 
 void Flow::calculate() {
+    vector< pair< int, int > > path = { };
+    int weight = 0;
+
+    while( bfs( path, weight, this->_graph) ){
+
+    }
+
     this->_maxFlow = 0;
 }
