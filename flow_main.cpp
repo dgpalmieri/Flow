7df90c93e08faf15aa48a53ee43a7e50813f5d6f
Dylan Palmieri <dgpalmieri@gmail.com>
2020-12-14 21:10:46 -0900

    Add textbook test case

--- flow_main.cpp
+++ flow_main.cpp
@@ -53,13 +53,13 @@ int main() {
         Flow basic_backwards_test( {
                 { { 1, 3 }, { 2, 2 } },
                 { { 2, 5 }, { 3, 2 } },
-                { { 3, 3} },
+                { { 3, 3 } },
                 { }
                 } );
 
         basic_backwards_test.calculate();
         int tFlow = basic_backwards_test.getMaxFlow();
-        cout << "Test Case: graph from Programiz example" << endl;
+        cout << "Test Case: graph from Geeks For Geeks small example" << endl;
         basic_backwards_test.printGraph();
         if ( tFlow != 5 ){
             cout << "   TEST FAILED" << endl;
@@ -71,6 +71,36 @@ int main() {
             cout << "   Test Passed!" << endl;
 
     } // end test case
+
+    { // test case
+      // From the course textbook, Introduction to the Design and Analysis of
+      // Algorithms, Levitin
+      // Figure 10.7, page 368
+      // NOTE: all node values are decreased by one, because in the book, the
+      // source is labeled as one, and here it is zero
+    
+        Flow textbook_test( {
+                { { 1, 2 }, { 3, 3 } },
+                { { 4, 3 }, { 2, 5 } },
+                { { 5, 2 } },
+                { { 2, 1 } },
+                { { 5, 4 } },
+                { }
+                } );
+
+        textbook_test.calculate();
+        int tFlow = textbook_test.getMaxFlow();
+        cout << "Test Case: graph from textbook example" << endl;
+        textbook_test.printGraph();
+        if ( tFlow != 3 ){
+            cout << "   TEST FAILED" << endl;
+            cout << "   Max Flow value calculated: " << tFlow << endl;
+            cout << "   Expected value: 6" << endl;
+
+        }
+        else
+            cout << "   Test Passed!" << endl;
+    }
 
     return 0;
 }
